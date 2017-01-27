33b640914cb9554e233dd46bf8453dddb46ec288
Paul Graffam <paulmgr@gmail.com>
2017-01-27 17:53:09 -0500

    Added lambda promisifier for testing

---
+++ serverless/lib/lambda-promisifier.js
@@ -0,0 +1,20 @@
+export function lambdaPromisifier(lambda) {
+  return options =>
+    new Promise((resolve, reject) =>
+      lambda(options, {
+        succeed: resolve,
+        fail: reject,
+        done: (err, res) => err ? reject(err) : resolve(res),
+        getRemainingTimeInMillis: () => Infinity,
+        functionName: 'fakeLambda',
+        functionVersion: '0',
+        invokedFunctionArn: 'arn:aws:lambda:fake-region:fake-acc:function:fakeLambda',
+        memoryLimitInMB: Infinity,
+        awsRequestId: 'fakeRequest',
+        logGroupName: 'fakeGroup',
+        logStreamName: 'fakeStream',
+        identity: null,
+        clientContext: null
+      })
+    );
+}
