f79351b7c656868fcccfaaf98956aa2013c41856
Paul Graffam <paulmgr@gmail.com>
2017-01-27 17:53:38 -0500

    Added json loader, added uglify plugin

--- serverless/webpack.config.js
+++ serverless/webpack.config.js
@@ -1,25 +1,29 @@
 var path = require('path');
 var fs = require('fs');
+var webpack = require('webpack');
 
 module.exports = {
   entry: fs.readdirSync(path.join(__dirname, './lambdas'))
            .filter(filename => /\.js$/.test(filename))
            .map((filename) => {
              var entry = {};
-             entry[filename.replace('.js', '')] = path.join(
-               __dirname,
-               './lambdas/',
-               filename
-             );
+             entry[filename.replace('.js', '')] = path.join(__dirname, './lambdas/', filename);
              return entry;
            })
-           .reduce((finalObject, entry) => Object.assign(finalObject, entry), {}),
+           .reduce((acc, entry) => Object.assign(acc, entry), {}),
   output: {
     path: path.join(__dirname, 'dist'),
     library: '[name]',
     libraryTarget: 'commonjs2',
     filename: '[name].js'
   },
+  //plugins: [
+  //  new webpack.optimize.UglifyJsPlugin({
+  //    compressor: {
+  //      warnings: false
+  //    }
+  //  })
+  //],
   target: 'node',
   module: {
     loaders: [
@@ -30,6 +34,10 @@ module.exports = {
         query: JSON.parse(
           fs.readFileSync(path.join(__dirname, '.babelrc'), { encoding: 'utf8' })
         )
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
       }
     ]
   }
