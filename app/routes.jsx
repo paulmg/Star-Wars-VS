46b387eee5d89335eac891bc57027a6d21ffe318
Paul Graffam <paulmgr@gmail.com>
2017-02-06 00:51:18 -0500

    Added redirect, Persons

--- app/routes.jsx
+++ app/routes.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute, IndexRedirect } from 'react-router';
 
 import Layout from './pages/Layout';
 import Main from './pages/Main';
 import Films from './pages/Films';
+import People from './pages/People';
 
 export default (
   <Route path="/" component={Layout}>
     <IndexRoute component={Main} />
+    <IndexRedirect to="/films" />
     <Route path="films" component={Films} />
+    <Route path="people" component={People} />
   </Route>
 );
